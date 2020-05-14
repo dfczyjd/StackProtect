f6879537ca11d33c4566eef2d0ee8a0387b0fe2f
dfczyjd <novak-vasil@yandex.ru>
2020-05-14 04:20:17 +0300

    Fixed x64

--- src/asm_x64.h
+++ src/asm_x64.h
@@ -5,15 +5,15 @@ const char* INIT_x64 = "\
 \tpushq\t$0x%x\n\
 %s\
 \taddq\t$4, %%rsp\n\
-\tmovq\t%%rax, %%rdi\n\
+\tmovq\t%%rax, %%r15\n\
 \tmovq\t$0, (%%rax)\n\
 ";
 
 // Код для замены инструкции call
 const char* CALL_x64 = "\
-\tmovq\t(%rdi), %rsi\n\
-\tincq\t%rsi\n\
-\tmovq\t%rsi, (%rdi)\n\
+\tmovq\t(%r15), %r14\n\
+\tincq\t%r14\n\
+\tmovq\t%r14, (%r15)\n\
 \tpushq\t$0\n\
 \tcall\tsave_ret\n\
 \tjmp\t\t\
@@ -21,12 +21,12 @@ const char* CALL_x64 = "\
 
 // Код для замены инструкции ret
 const char* RET_x64 = "\
-\tmovq\t(%rdi), %rsi\n\
-\tdecq\t%rsi\n\
-\tmovq\t%rsi, (%rdi)\n\
-\tincq\t%rsi\n\
+\tmovq\t(%r15), %r14\n\
+\tdecq\t%r14\n\
+\tmovq\t%r14, (%r15)\n\
+\tincq\t%r14\n\
 \tsubq\t$0x4, %rsp\n\
-\tjmp\t\t*(%rdi,%rsi,4)\n\
+\tjmp\t\t*(%r15,%r14,4)\n\
 ";
 
 // Код вызова malloc() для UNIX x64
@@ -34,11 +34,13 @@ const char* UNIX_MALLOC_x64 = "\tcall\tmalloc@PLT\n";
 
 // Код функции save_ret
 const char* SAVE_RET_x64 = "\
+.LB:\n\
+\t.text\n\
 save_ret:\n\
 \tpushq\t%rax\n\
 \tmovq\t0x4(%rsp), %rax\n\
 \taddq\t$0x5, %rax\n\
-\tmovq\t%rax, (%rdi, %rsi, 4)\n\
+\tmovq\t%rax, (%r15, %r14, 4)\n\
 \tpopq\t%rax\n\
 \tret\n\
 ";
